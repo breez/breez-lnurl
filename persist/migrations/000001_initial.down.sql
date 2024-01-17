e29525a93d045161d33ae8e07ad6b9997c7acba3
Roei Erez <roeierez@gmail.com>
2024-01-18 00:02:33 +0200

    fix down migration

--- persist/migrations/000001_initial.down.sql
+++ persist/migrations/000001_initial.down.sql
@@ -1,4 +1,3 @@
-DROP INDEX if exists lnurl_webhooks_pubkey_url_key;
 DROP INDEX if exists lnurl_webhooks_pubkey_hook_key_hash_key;
 DROP INDEX if exists lnurl_webhooks_pubkey_idx;
 DROP TABLE if exists public.lnurl_webhooks;
